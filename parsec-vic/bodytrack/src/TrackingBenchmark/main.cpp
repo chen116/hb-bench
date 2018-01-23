704d6ec022082fb4bfa23ff8d5115bc75c06e8ed
Victor Chen <chen116@usc.edu>
2018-01-22 17:06:25 -0800

    Update main.cpp

--- parsec-vic/bodytrack/src/TrackingBenchmark/main.cpp
+++ parsec-vic/bodytrack/src/TrackingBenchmark/main.cpp
@@ -33,7 +33,7 @@ heartbeat_t* heart;
 
 static const int64_t vic_win_size = 10;
 static const int64_t vic_buf_depth = 1000;
-static const char* vic_log_file ="vic-stream.log";
+static const char* vic_log_file ="vic.log";
 static const int64_t vic_min_target = 100;
 static const int64_t vic_max_target = 1000;
 #include <vector>
